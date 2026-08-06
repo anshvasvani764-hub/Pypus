bd73cbb77a8f7c50d23fa4111242ad65b5eec73d
ansh <anshvasvani764@gmail.com>
2026-08-06 16:07:01 +0530

    Update sitemap URL to include 'www'

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://pypus.in',
+      url: 'https://www.pypus.in',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
