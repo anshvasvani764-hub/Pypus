b0c6cf1883cf71203bb74d30da4974edeac4c8fe
ansh <anshvasvani764@gmail.com>
2026-08-06 15:57:17 +0530

    Add sitemap function for site metadata

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://pypus.in',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://pypus.in/login',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+  ]
+}
